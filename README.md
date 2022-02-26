d211bb07e946811e40fe1cadab3465949b1062c7
Kobey Pabellan <pabellank@gmail.com>
2022-02-25 23:41:25 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Krispy Kreme Generator
+
+Generates Krispy Kreme accounts with next day birthday, allowing users to get free donuts.
+
+### Requirements
+- [2Captcha](https://2captcha.com/)
+- Catchall
+
+### Configuration
+1. Open `example.env`.
+2. Input 2Captcha API key and catchall domain.
+3. Rename file to `.env`.
---
+++ main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+
+	chromedp "github.com/chromedp/chromedp"
+	"github.com/joho/godotenv"
+	api2captcha "github.com/kpabellan/2captcha-go"
+)
+
+var waitGroup sync.WaitGroup
+
+var captchaClientKey string = goDotEnvVariable("CAPTCHACLIENTKEY")
+
+var catchall string = goDotEnvVariable("CATCHALL")
+
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+func goDotEnvVariable(key string) string {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+func randomCharNumerals(length int) string {
+	const charset = "0123456789"
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func runFunc(timeout time.Duration, task chromedp.ActionFunc) chromedp.ActionFunc {
+	return func(ctx context.Context) error {
+		ctx, cancel := context.WithTimeout(ctx, timeout)
+		defer cancel()
+
+		return task.Do(ctx)
+	}
+}
+
+func runTask(timeout time.Duration, tasks chromedp.Tasks) chromedp.ActionFunc {
+	return func(ctx context.Context) error {
+		timeoutContext, cancel := context.WithTimeout(ctx, timeout)
+		defer cancel()
+
+		return tasks.Do(timeoutContext)
+	}
+}
+
+func solveReCaptcha(client *api2captcha.Client, kkUrl, dataSiteKey string) (string, error) {
+	c := api2captcha.ReCaptcha{
+		SiteKey:   dataSiteKey,
+		Url:       kkUrl,
+		Invisible: true,
+		Action:    "verify",
+	}
+
+	return client.Solve(c.ToRequest())
+}
+
+func generateKK() {
+	defer waitGroup.Done()
+	client := api2captcha.NewClient(captchaClientKey)
+	kkUrl := "https://www.krispykreme.com/account/create-account"
+	tomorrow := time.Now().AddDate(0, 0, 1)
+	tomorrowDay := strconv.Itoa(tomorrow.Day())
+	tomorrowMonth := strconv.Itoa(int(tomorrow.Month()))
+	var siteKey string
+	var siteKeyOk bool
+
+	if len(tomorrowMonth) == 1 {
+		tomorrowMonth = "0" + tomorrowMonth
+	}
+
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"),
+	)
+
+	cx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancel()
+
+	ctx, cancel := chromedp.NewContext(cx)
+	defer cancel()
+
+	if err := chromedp.Run(ctx,
+		chromedp.Navigate(kkUrl),
+		chromedp.AttributeValue(`[data-sitekey]`, "data-sitekey", &siteKey, &siteKeyOk),
+		chromedp.Click("#cookie-consent-container > div.cta > a.btn.btn-secondary.btn-ok.shadow"),    // Accept cookies
+		chromedp.Evaluate(`document.querySelector("#btnSubmit").removeAttribute("disabled")`, nil),   // Enable the submit button
+		chromedp.SetAttributeValue("#btnSubmit", "class", "btn", chromedp.ByQuery),                   // Change submit button class to btn
+		chromedp.SendKeys("#ctl00_plcMain_txtFirstName", "Krispy"),                                   // Input first name
+		chromedp.SendKeys("#ctl00_plcMain_txtLastName", "Kreme"),                                     // Input last name
+		chromedp.SendKeys("#ctl00_plcMain_ddlBirthdayMM", tomorrowMonth),                             // Input birthday month
+		chromedp.SendKeys("#ctl00_plcMain_ddlBirthdayDD", tomorrowDay),                               // Input birthday day
+		chromedp.SendKeys("#ctl00_plcMain_txtZipCode", "90001"),                                      // Input zipcode
+		chromedp.SendKeys("#ctl00_plcMain_ucPhoneNumber_txt1st", randomCharNumerals(3)),              // Input phone number area code
+		chromedp.SendKeys("#ctl00_plcMain_ucPhoneNumber_txt2nd", randomCharNumerals(3)),              // Input phone number first 3 digits
+		chromedp.SendKeys("#ctl00_plcMain_ucPhoneNumber_txt3rd", randomCharNumerals(4)),              // Input phone number last 4 digits
+		chromedp.SendKeys("#ctl00_plcMain_txtEmail", "kp"+randomCharNumerals(5)+"@"+catchall+".com"), // Input email
+		chromedp.SendKeys("#ctl00_plcMain_txtPassword", "Donuttime123"),                              // Input password
+		chromedp.Click("#ctl00_plcMain_cbTermsOfUse"),                                                // Click terms of use
+		runFunc(5*time.Minute, func(ctx context.Context) error {
+			if !siteKeyOk {
+				return errors.New("missing data-sitekey")
+			}
+
+			token, err := solveReCaptcha(client, kkUrl, siteKey)
+			if err != nil {
+				return err
+			}
+
+			return chromedp.SetJavascriptAttribute(`#g-recaptcha-response`, "innerText", token).Do(ctx)
+		}),
+
+		chromedp.Click("#btnSubmit", chromedp.ByQuery), // Submit form
+		runTask(3*time.Minute, chromedp.Tasks{
+			chromedp.WaitVisible(`a[href*="twitter"]`, chromedp.ByQuery),
+		}),
+	); err != nil {
+		fmt.Printf("Krispy Kreme Generation Failed. (%s)\n", err)
+	} else {
+		fmt.Printf("Krispy Kreme Generation Success!\n")
+	}
+}
+
+func main() {
+	var genAmount int
+	fmt.Print("Enter amount to generate: ")
+	fmt.Scanf("%d", &genAmount)
+	fmt.Printf("Generating %d Krispy Kreme accounts...\n", genAmount)
+
+	for i := 0; i < genAmount; i++ {
+		go generateKK()
+		waitGroup.Add(1)
+	}
+
+	waitGroup.Wait()
+}
